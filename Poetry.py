0221104b3d2ffdf7c7de98d93f5b8aef8dd99a46
Ben Simpson <b.albertsimpson@gmail.com>
2019-11-07 16:29:59 -0800

    updates for random lines

--- Poetry.py
+++ Poetry.py
@@ -1,12 +1,11 @@
-poem = '''
-If you are a dreamer, come in,
+from random import random, randint
+poem = '''If you are a dreamer, come in,
 If you are a dreamer, a wisher, a liar,
 A hope-er, a pray-er, a magic bean buyer…
 If you’re a pretender, come sit by my fire
 For we have some flax-golden tales to spin.
 Come in!
-Come in!
-'''
+Come in!'''
 
 my_poem = []
 
@@ -30,10 +29,15 @@ def lines_printed_random():
     '''will randomly select lines from a list of strings and print them out in random order. 
     Repeats are okay and the number of lines printed should be equal to the original number of lines in the poem (line numbers don't need to be printed). 
     Hint: try using a loop and randint()'''
+
+    random_lines = len(lines)
+    for i in range(len(lines)):
+        line_index = randint(1, random_lines-1)
+        print(lines[line_index])
 def my_own_rearrange():
     pass
 
-
+lines_printed_random()
 #lines_printed_backwards(lines)
 #Testing code
 #lines_printed_backwards(poem_lines_list)
